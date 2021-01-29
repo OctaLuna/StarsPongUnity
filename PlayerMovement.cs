94c47d10cce3ad4523b6357e9dfc12db3cab372b
Marco Octavio Luna Vargas <marco.octavio.luna@gmail.com>
2021-01-29 12:16:13 -0400

    Commit 0, se agregaron 2 script, uno llamado PlayerController que ya esta programado y se agrego BallController para controlar la pelota

---
+++ PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //VARIABLES//
+    //estas variables es para poner restricciones en los bordes del la camara para que el player solo se vea en pantalla
+    public float UpRestri;
+    public float DownRestri;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 MousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(MousePosition.y, DownRestri, UpRestri), transform.position.z);
+    }
+}
