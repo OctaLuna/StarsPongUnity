09311c114756314b5f64220cb3f364fc640fa0bf
Marco Octavio Luna Vargas <marco.octavio.luna@gmail.com>
2021-01-30 17:59:19 -0400

    Commit 2, se agrego el controlador de escena, llamado SceneController

---
+++ SceneController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;      //Es el controlador de las escenas
+
+public class SceneController : MonoBehaviour
+{
+    public void ChangeSceneTo(string sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
+}
