1a7b111f6c51f30c94e65fb6186864b718a3d414
Marco Octavio Luna Vargas <marco.octavio.luna@gmail.com>
2021-01-30 16:56:07 -0400

    Commit 1, se programo el movimiento de la pelota, y se agrego y programo a Player2Movement pero aun no tiene parametros de Up y Down

--- BallController.cs
+++ BallController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //VARIABLES 
+    //Posicion de el player 
+    public Transform PlayerPosition;
+    //Esta es la variable para que inicie el juego
+    private bool GameStar = false;
+    //esto hace una referencia a el RigidBody de la pelota 
+    public Rigidbody2D RBBall;
+    //Velocidad de la pelota en "x" y "y"
+    public float SpeedBallX = 30;
+    public float SpeedBallY = 30;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(GameStar == false){
+            float PositionBallx = PlayerPosition.transform.position.x - transform.position.x;
+
+            transform.position = new Vector3(PlayerPosition.position.x - PositionBallx, PlayerPosition.position.y, transform.position.z);
+
+            if(Input.GetMouseButtonDown(0)){
+                RBBall.velocity = new Vector2(15, 15);
+                GameStar = true;
+            }
+        }        
     }
 }
---
+++ Player2Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Movement : MonoBehaviour
+{
+    //VARIABLES//
+    //speed
+    public float Speed;
+    //es la variable que hace referencia a el movimiento Vertical de un objeto
+    private const string Vertical = "Vertical";
+    //Restricciones de movimiento vertical
+    public float UpRestri;
+    public float DownRestri;
+
+
+    // Update is called once per frame
+    void Update()
+    { 
+        float PlayerController = Input.GetAxisRaw(Vertical) * Speed * Time.deltaTime;
+
+        this.transform.Translate(new Vector3(0, Mathf.Clamp(PlayerController, UpRestri, DownRestri), 0));
+        //transform.position = new Vector3(transform.position.x, Input.GetAxisRaw(Vertical) * Speed * Time.deltaTime, transform.position.z);
+
+    }
+}
