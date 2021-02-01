14f96410c9fb4fbd2e7eb5cb117d2ac9f915e2fb
Marco Octavio Luna Vargas <marco.octavio.luna@gmail.com>
2021-02-01 12:21:59 -0400

    Se programo al enemigo del juego, el script se llama EnemyController

--- EnemyController.cs
+++ EnemyController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //VARIABLES//
+    //Esta variable hace referencia a el trasform de la pelota
+    public Transform Ball;
+    //Esta es la velocidad de el  enemigo
+    public float Speed;
+
 
     // Update is called once per frame
     void Update()
     {
+        
+        if(transform.position.y < Ball.position.y){
+            transform.position = new Vector3(transform.position.x, transform.position.y + Speed, transform.position.z);
+        }
+        else if(transform.position.y > Ball.position.y){
+            transform.position = new Vector3(transform.position.x, transform.position.y - Speed, transform.position.z);
+        }
         
     }
 }
